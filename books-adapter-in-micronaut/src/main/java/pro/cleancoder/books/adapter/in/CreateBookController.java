7c48dbdc0b311e334f43dbf79ff94249e0db78cc
Radek Starczynowski <radek.starczynowski@gmail.com>
2022-02-14 16:45:09 +0100

    Micronaut app

---
+++ books-adapter-in-micronaut/src/main/java/pro/cleancoder/books/adapter/in/CreateBookController.java
@@ -0,0 +1,24 @@
+package pro.cleancoder.books.adapter.in;
+
+import io.micronaut.http.annotation.Body;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Post;
+import lombok.RequiredArgsConstructor;
+import pro.cleancoder.books.port.in.CreateBookCommand;
+import pro.cleancoder.books.port.in.CreateBookUseCase;
+
+import java.util.UUID;
+
+@Controller("/books")
+@RequiredArgsConstructor
+public class CreateBookController {
+
+    private final CreateBookUseCase createBookUseCase;
+
+    @Post
+    public UUID createBook(@Body CreateBookResource resource) {
+        var command = new CreateBookCommand(resource.author(), resource.title(), resource.gender());
+        return createBookUseCase.createBook(command);
+    }
+
+}
---
+++ books-configuration-micronaut/src/main/java/pro/cleancoder/ApplicationConfiguration.java
@@ -0,0 +1,26 @@
+package pro.cleancoder;
+
+import io.micronaut.context.annotation.Bean;
+import io.micronaut.context.annotation.Factory;
+import pro.cleancoder.books.BookMapper;
+import pro.cleancoder.books.BooksRepository;
+import pro.cleancoder.books.port.in.CreateBookUseCase;
+import pro.cleancoder.books.port.out.CreateBookPort;
+import pro.cleancoder.books.service.CreateBookService;
+
+import javax.sql.DataSource;
+
+@Factory
+class ApplicationConfiguration {
+
+    @Bean
+    public CreateBookPort createBookPort(DataSource dataSource) {
+        return new BooksRepository(dataSource, new BookMapper());
+    }
+
+    @Bean
+    public CreateBookUseCase createBookUseCase(CreateBookPort createBookPort) {
+        return new CreateBookService(createBookPort);
+    }
+
+}
